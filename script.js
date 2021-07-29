011368ec33111880544101c87fbb98c255f03530
Limarychard <ry.lima2708@icloud.com>
2021-07-29 17:00:29 -0300

    finishing my calculator

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 const insertValue = (num) => {
   const input = document.getElementById('input-value').value
-  document.getElementById('input-value').value = input + num 
+  document.getElementById('input-value').value = input + num
 }
 
 const calculate = () => {
-    const input = document.getElementById('input-value').value
-    document.getElementById('input-value').value = eval(input)
+  const input = document.getElementById('input-value').value
+  document.getElementById('input-value').value = eval(input)
+}
+
+const clearInput = () => {
+  document.getElementById('input-value').value = " "
+}
+
+const invertValue = () => {
+  calculate()
+
+  const input = document.getElementById('input-value').value
+  document.getElementById('input-value').value = input * -1
+}
+
+const percent = (num) => {
+  calculate()
+
+  const input = document.getElementById('input-value').value
+  document.getElementById('input-value').value = eval(input) / 100
 }
